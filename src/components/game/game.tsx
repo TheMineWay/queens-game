536cd24b94ba34ee34b54e8ea2eadf97db5e3f72
Joel Campos <joelcamposoliva9000@gmail.com>
2024-09-17 20:06:08 +0200

    feat: Game mock

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,5 +1,19 @@
 import Board from "@/components/game/board/board";
 
 export default function Game() {
-  return <Board board={{ board: [], colors: [] }} />;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <Board
+        board={{
+          board: [
+            [1, 0, 1, 0],
+            [0, 1, 0, 1],
+            [1, 0, 1, 0],
+            [0, 1, 0, 1],
+          ],
+          colors: ["FF00FF", "12BA12"],
+        }}
+      />
+    </div>
+  );
 }
