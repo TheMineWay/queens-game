5ee5e8d3b31a85e71031e976ccf4cb57fd6e66de
Joel Campos <joelcamposoliva9000@gmail.com>
2024-09-17 22:05:26 +0200

    feat: timer hook and format utility

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import Board from "@/components/game/board/board";
+import Timer from "@/components/game/timer/timer";
+import { useTimer } from "@/hooks/utils/use-timer";
 
 export default function Game() {
+  const timer = useTimer();
+
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex flex-col gap-4 justify-center items-center">
+      <Timer timer={timer} />
       <Board
         board={{
           board: [
